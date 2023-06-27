54dc72a9e912bc650db84dde511e659c46cc897a
mvrcentes <mvrcentes@hotmail.com>
2023-06-27 11:42:59 -0600

    Setup project

---
+++ jarbo/lib/pages/homePage.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Color.fromARGB(255, 136, 47, 47),
+      body: Center(
+        child: Text(
+          'Hello, World!',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 24,
+          ),
+        ),
+      ),
+    );
+  }
+}
